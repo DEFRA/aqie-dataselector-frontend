359c83cab61ee0900f18472c4ae4818a35b1e2ec
Sruthi-Sunder <Sruthi.Sundararajan@defra.gov.uk>
2025-03-12 18:52:56 +0000

    Sprint 7_scss

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,13 +5,13 @@ export default {
   extends: ['stylelint-config-gds/scss'],
   ignoreFiles: ['**/public/**', '**/package/**', '**/vendor/**'],
   rules: {
-    "scss/double-slash-comment-whitespace-inside": "always",
-    "length-zero-no-unit": null,
-    "rule-empty-line-before": null,
-    "declaration-block-no-duplicate-properties": null,
-    "color-named": null,
-    "declaration-no-important": null,
-    "shorthand-property-no-redundant-values": null
+    'scss/double-slash-comment-whitespace-inside': 'always',
+    'length-zero-no-unit': null,
+    'rule-empty-line-before': null,
+    'declaration-block-no-duplicate-properties': null,
+    'color-named': null,
+    'declaration-no-important': null,
+    'shorthand-property-no-redundant-values': null
   }
 }
 
