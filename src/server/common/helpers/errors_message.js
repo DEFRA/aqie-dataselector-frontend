7e143a66c93043133f4e1038dfd857bce09eab38
SruthiS-CTS <Sruthi.Sundararajan@cognizant.com>
2025-08-22 11:49:05 +0100

    Errorhandling

--- src/server/common/helpers/errors_message.js
+++ src/server/common/helpers/errors_message.js
@@ -28,7 +28,7 @@ function catchAll(request, h) {
   request.logger.error(response?.stack)
   // return  response.output
   return h.redirect(
-    '/check-plant-health-information-and-import-rules/problem-with-service?statusCode=' +
+    '/check-air-quality/problem-with-service?statusCode=' +
       response.output.statusCode
   )
 }
