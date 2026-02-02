46493385ccdc98c115bfa3c16e9c2479a7394225
SruthiS-CTS <Sruthi.Sundararajan@cognizant.com>
2026-02-02 14:25:36 +0000

    sessionerrs

--- src/server/hubpage/controller.js
+++ src/server/hubpage/controller.js
@@ -14,8 +14,28 @@ export const hubController = {
     request.yar.set('osnameapiresult', '')
     request.yar.set('selectedLocation', '')
     request.yar.set('nooflocation', '')
+    request.yar.set('nooflocation', '')
     request.yar.set('yearselected', new Date().getFullYear().toString())
     request.yar.set('selectedYear', new Date().getFullYear().toString())
+    request.yar.set('selectedpollutant', '')
+    request.yar.set('selectedyear', '')
+    request.yar.set('selectedlocation', '')
+    request.yar.set('nooflocation', '')
+
+    // Clear pollutant-specific session variables
+    request.yar.set('selectedPollutants', null)
+    request.yar.set('selectedPollutantMode', '')
+    request.yar.set('selectedPollutantGroup', '')
+    request.yar.set('formattedPollutants', '')
+
+    // Clear other related session variables including time period
+    request.yar.set('selectedTimePeriod', null)
+    request.yar.set('yearrange', '')
+    request.yar.set('finalyear', '')
+    request.yar.set('finalyear1', '')
+    request.yar.set('Region', '')
+    request.yar.set('selectedLAIDs', '')
+    request.yar.set('Location', '')
 
     return h.view('hubpage/index', {
       pageTitle: englishNew.hub.pageTitle,
