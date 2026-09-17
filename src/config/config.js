4f7f91e474ffeddaf2f899224690c5568cd7ed34
SruthiS-CTS <Sruthi.Sundararajan@cognizant.com>
2026-09-17 17:10:05 +0100

    Sept17

--- src/config/config.js
+++ src/config/config.js
@@ -299,7 +299,7 @@ export const config = convict({
   DevApiKey: {
     doc: 'Common API key for all dev endpoints (localhost only)',
     format: String,
-    default: '6c4HZdeRiWzL6mBp0Y7X458DNZDfhpbi',
+    default: '',
     sensitive: true,
     env: 'OS_NAMES_DEV_API_KEYS'
   },
